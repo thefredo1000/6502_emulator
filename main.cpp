48149d4bfa5a1f8bd984978207eb220b4ca0fd3b
RodrigoCasale <popobasura3@hotmail.com>
2021-03-29 15:09:10 -0600

    Mem init created

--- main.cpp
+++ main.cpp
@@ -5,11 +5,24 @@
 // http://www.obelisk.me.uk/6502/
 
 
+using Byte = unsigned char;
+using Word = unsigned short;
 
+using u32 = unsigned int;
+
+struct Mem {
+    static constexpr u32 MAX_MEM = 1024 * 64;
+    Byte data[MAX_MEM];
+
+    void init() {
+        for ( u32 i = 0; i < MAX_MEM; i++) {
+            data[i] = 0;
+        }
+    }
+
+};
 
 struct CPU {
-    using Byte = unsigned char;
-    using Word = unsigned short;
 
     Word PC;    // Program Counter
     Word SP;    // Stack Pointer
@@ -24,17 +37,25 @@ struct CPU {
     Byte V : 1; // Status Flag
     Byte N : 1; // Status Flag
 
-    void reset() {
+    void reset( Mem& memory ) {
         PC = 0xFFFC;
         SP = 0x0100;
         C = Z = I = D = B = V = N = 0;
         A = X = Y = 0;
+
+        memory.init();
+    }
+
+    void exec( u32 cycles, Mem& memory ) {
+        
     }
 };
 
 int main() {
 
+    Mem mem;
     CPU cpu;
-    cpu.reset();
+    cpu.reset(mem);
+    cpu.exec( 2, mem );
     return 0;
 }
