8469c595574b8878272b50c2b9c49f7f25c194eb
RodrigoCasale <popobasura3@hotmail.com>
2021-03-30 20:51:41 -0600

    LDA ZP

--- main.cpp
+++ main.cpp
@@ -25,6 +25,12 @@ struct Mem {
         // assert here address is < MAX_MEM
         return data[address];
     } 
+
+    /** Write 1 byte */
+    Byte& operator[] (u32 address )  {
+        // assert here address is < MAX_MEM
+        return data[address];
+    } 
 };
 
 struct CPU {
@@ -51,16 +57,50 @@ struct CPU {
         memory.init();
     }
 
-    Byte fetchByte (u32 cycles, Mem& memory) {
+    Byte fetchByte (u32& cycles, Mem& memory) {
         Byte data = memory[PC];
         PC++;
         cycles--;
         return data;
     }
 
+    Byte readByte (u32& cycles, Byte address, Mem& memory) {
+        Byte data = memory[address];
+        cycles--;
+        return data;
+    }
+    // Opcodes
+    static constexpr Byte INS_LDA_IM = 0xA9;
+    static constexpr Byte INS_LDA_ZP = 0xA5;
+
+    void LDASetStatus() {
+        Z = (A == 0);
+        N = (A & 0b10000000) > 0;
+    }
+
     void exec( u32 cycles, Mem& memory ) {
         while (cycles > 0) {
             Byte instr = fetchByte(cycles, memory);
+
+            switch (instr){
+                case INS_LDA_IM :{
+                    Byte val = fetchByte (cycles, memory);
+                    A = val;
+                    LDASetStatus();
+                    printf("RAN \n");
+                } 
+                break;
+                case INS_LDA_ZP :{
+                    Byte zeroPageAddr = fetchByte (cycles, memory);
+                    A = readByte( cycles, zeroPageAddr, memory);
+
+                } 
+                break;
+                default: {
+                    cycles--;
+                    printf("Instruction not handled %x \n", instr);
+                } break;
+            }
         }
     }
 };
@@ -69,7 +109,10 @@ int main() {
 
     Mem mem;
     CPU cpu;
-    cpu.reset(mem);
-    cpu.exec( 2, mem );
+    cpu.reset( mem );
+    mem[0xFFFC] = CPU::INS_LDA_ZP;
+    mem[0xFFFD] = 0x42;
+    mem[0x0042] = 0x84;
+    cpu.exec( 3, mem );
     return 0;
 }
